b7d211523174b5d9aeae0b1c563c9f91b760854e
Ahmed Magdy <ahmedahmedmagdy491@gmail.com>
2020-12-15 22:04:30 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import NB from "./Component/Navbar";
+import Caro from "./Component/Carousel";
+import Recent from "./Component/recently";
+import { Col, Container, Row } from "react-bootstrap";
+import Kawkab from "./Component/kawkab";
+import Shakhsiat from "./Component/shakhsiat";
+import line from './img/Line 3.svg'
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div>
+			<NB className="navbar" />
+			<Container variant="fluid">
+				<Caro />
+				<Row>
+					<img src={line} alt="line" className="line" />
+				</Row>
+				<Row>
+					
+						<Kawkab />
+					
+						<Recent />
+					
+				</Row>
+				<img src={line} alt="line" className="line" />
+				
+				<Row><Shakhsiat /></Row>
+			</Container>
+		</div>
+	);
 }
 
 export default App;
